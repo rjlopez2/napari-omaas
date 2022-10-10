1521fecf1b7d1303205dbdf00419156402811402
Ruben Lopez <rjlopez2@gmail.com>
2022-10-10 15:39:39 +0200

    clean and create the basis for the layout canvas

--- src/napari_omaas/__init__.py
+++ src/napari_omaas/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.1.0"
 
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, example_magic_widget
+from ._widget import Omass, example_magic_widget
 from ._writer import write_multiple, write_single_image
 
 __all__ = (
@@ -10,6 +10,6 @@ __all__ = (
     "write_single_image",
     "write_multiple",
     "make_sample_data",
-    "ExampleQWidget",
+    "Omass",
     "example_magic_widget",
 )
--- src/napari_omaas/_widget.py
+++ src/napari_omaas/_widget.py
@@ -9,13 +9,13 @@ Replace code below according to your needs.
 from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from qtpy.QtWidgets import QHBoxLayout, QVBoxLayout, QTabWidget, QPushButton, QWidget, QGroupBox, QGridLayout
 
 if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
+class Omass(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
@@ -24,14 +24,77 @@ class ExampleQWidget(QWidget):
         super().__init__()
         self.viewer = napari_viewer
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        # self.btn = QPushButton("Click me!")
+        # self.btn.clicked.connect(self._on_click)
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        # self.setLayout(QHBoxLayout())
+        # self.layout().addWidget(btn)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        ##################################################
+        self.main_layout = QVBoxLayout()
+        self.setLayout(self.main_layout)
+
+        self.tabs = QTabWidget()
+        self.main_layout.addWidget(self.tabs)
+
+        # Analysis tab
+        self.analysis = QWidget()
+        self._analysis_layout = QVBoxLayout()
+        self.analysis.setLayout(self._analysis_layout)
+        self.tabs.addTab(self.analysis, 'Analysis')
+
+        # aquisition tab
+        self.aquisition_tab = QWidget()
+        self._aquisition_tab_layout = QVBoxLayout()
+        self.aquisition_tab.setLayout(self._aquisition_tab_layout)
+        self.tabs.addTab(self.aquisition_tab, 'Aquisition')
+
+        # self.layout().addWidget(btn)
+
+
+
+        #/////// analysis tab /////////
+        # adding the plotting panel
+        self.plotting_panel = VHGroup('Visualize image profile', orientation='G')
+        self._analysis_layout.addWidget(self.plotting_panel.gbox)
+
+        # add button for testing
+        self.btn = QPushButton("Click me!")
+        self.plotting_panel.glayout.addWidget(self.btn, 0, 0, 1, 2)
+        # self.btn.clicked.connect(self._on_click)
+
+        # adding the Analysis panel
+        self.analysis_panel = VHGroup('Analysis', orientation='G')
+        self._analysis_layout.addWidget(self.analysis_panel.gbox)
+        self.normalize_btn = QPushButton("Normalize Image")
+        self.analysis_panel.glayout.addWidget(self.normalize_btn, 0, 0, 1, 2)
+
+
+
+
+
+
+
+# Add at the end the connections
+        self.add_connections()
+
+# define the callbacks function
+
+    def add_connections(self):
+         """Add callbacks"""
+         self.btn.clicked.connect(self._on_click_test_btn)
+
+
+
+# define teh logic for each button
+    def _on_click_test_btn(self):
+        print("napari has", len(self.viewer.layers), "layers_lalala")
+
+
+
+
+
+
 
 
 @magic_factory
@@ -44,3 +107,33 @@ def example_magic_widget(img_layer: "napari.layers.Image"):
 # a widget.
 def example_function_widget(img_layer: "napari.layers.Image"):
     print(f"you have selected {img_layer}")
+
+
+
+
+
+
+
+class VHGroup():
+    """Group box with specific layout.
+
+    Parameters
+    ----------
+    name: str
+        Name of the group box
+    orientation: str
+        'V' for vertical, 'H' for horizontal, 'G' for grid
+    """
+
+    def __init__(self, name, orientation='V'):
+        self.gbox = QGroupBox(name)
+        if orientation=='V':
+            self.glayout = QVBoxLayout()
+        elif orientation=='H':
+            self.glayout = QHBoxLayout()
+        elif orientation=='G':
+            self.glayout = QGridLayout()
+        else:
+            raise Exception(f"Unknown orientation {orientation}") 
+
+        self.gbox.setLayout(self.glayout)
