eb9359d515869e0e11a842a0cbf3460f503d3618
Ruben Lopez <rjlopez2@gmail.com>
2022-11-12 00:52:58 +0100

    adding basic saving ROIs implementation

--- src/napari_omaas/_widget.py
+++ src/napari_omaas/_widget.py
@@ -9,7 +9,7 @@ Replace code below according to your needs.
 from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget, QFileDialog
 
 from .utils import *
 
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
+class OMAAS(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
@@ -26,38 +26,48 @@ class ExampleQWidget(QWidget):
         super().__init__()
         self.viewer = napari_viewer
 
+        # create buttons
         inv_data_btn = QPushButton("invert image")
         norm_data_btn = QPushButton("normalize image")
+
+        load_ROIs_btn = QPushButton("load ROIs")
+        save_ROIs_btn = QPushButton("Save ROIs")
 
 
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(inv_data_btn)
         self.layout().addWidget(norm_data_btn)
+        self.layout().addWidget(load_ROIs_btn)
+        self.layout().addWidget(save_ROIs_btn)
         
         # callbacks
         inv_data_btn.clicked.connect(self._on_click_inv_data_btn)
         norm_data_btn.clicked.connect(self._on_click_norm_data_btn)
+        load_ROIs_btn.clicked.connect(self._on_click_load_ROIs_btn)
+        save_ROIs_btn.clicked.connect(self._on_click_save_ROIs_btn)
 
     def _on_click_inv_data_btn(self):
         results =invert_signal(self.viewer.layers.selection)
-        # name= f'{self.viewer.layers.layers.Image}_norm')
-        # self.viewer._add_layer_from_data(results)
         self.viewer.add_image(results, 
         colormap= "twilight_shifted", 
-        # gamma= 0.2,
         name= f"{self.viewer.layers.selection.active}_Inv")
-        # invert_signal(self.viewer.layers.selection)
 
     def _on_click_norm_data_btn(self):
-
         results = local_normal_fun(self.viewer.layers.selection)
-
-
         self.viewer.add_image(results, 
         colormap= "twilight_shifted", 
-        # gamma= 0.2,
         name= f"{self.viewer.layers.selection.active}_Nor")
 
+    def _on_click_load_ROIs_btn(self, event=None, filename=None):
+        if filename is None: filename, _ = QFileDialog.getOpenFileName(self, "Load ROIs", ".", "ImageJ ROIS(*.roi *.zip)")
+        self.viewer.open(filename, plugin='napari_jroireader')
+        
+    
+    def _on_click_save_ROIs_btn(self, event=None, filename=None):
+        if filename is None: filename, _ = QFileDialog.getSaveFileName(self, "Save as .csv", ".", "*.csv")
+        # self.viewer.layers.save(filename, plugin='napari_jroiwriter')
+        self.viewer.layers.save(filename, plugin='napari')
+    
 
 
 
