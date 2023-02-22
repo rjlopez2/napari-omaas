030172f4f359532e3ecf32b34c548b7050c6193b
Ruben Lopez <rjlopez2@gmail.com>
2023-02-22 15:45:04 +0100

    refactor VScode debugging test file

--- src/test.py
+++ src/test.py
@@ -7,8 +7,8 @@ import napari_omaas
 # importlib.reload(myModule)
 # from myModule import *
 
-t = 70
-print(t)
+# t = 70
+# print(t)
 # viewer = napari.view_image(data.cells3d(), channel_axis=1, ndisplay=3)
 
 # viewer.open("12h-49m-26s.sif", plugin="napari-"  )
@@ -16,4 +16,9 @@ viewer = napari.Viewer()
 o = napari_omaas.OMAAS(viewer)
 # widg = napari_omaas.OMAAS.(viewer=viewer)  # or any other QWidget
 viewer.window.add_dock_widget(o, area='right')
+viewer.open("/Users/rubencito/programming_stuff/p_experimental/image_analysis/napari_play/OMAAS/test_data/11h-27m-32s.sif", plugin= "napari-sif-reader")
+
+
 napari.run()  # start the "event loop" and show the viewer
+
+print("end")
