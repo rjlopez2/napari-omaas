6a77d6fa32c4bd2862857a227de73323418c3220
Ruben Lopez <rjlopez2@gmail.com>
2023-04-19 15:20:13 +0200

    create read_sample function

--- src/napari_omaas/_sample_data.py
+++ src/napari_omaas/_sample_data.py
@@ -9,13 +9,24 @@ Replace code below according to your needs.
 from __future__ import annotations
 
 import numpy
+from napari_sif_reader import napari_get_reader
+
+import os
+import sys
+
+THIS_DIR = os.path.dirname(__file__)
+# sys.path.append(THIS_DIR + '/../../img/' )
+DATA_DIR = THIS_DIR + '/../../img/'
+
+# sample_img_dir = THIS_DIR + '/../../img/' 
 
 
 def make_sample_data():
+
     """Generates an image"""
-    # Return list of tuples
-    # [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
-    # Check the documentation for more information about the
-    # add_image_kwargs
-    # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
-    return [(numpy.random.rand(512, 512), {})]
+    path = DATA_DIR + '11h-27m-32s.sif'
+    sif_reader_fun = napari_get_reader(path)
+    img_layer = sif_reader_fun(path)
+    img_layer[0][1]["name"] = 'TestImg_11h-27m-32s'
+
+    return img_layer
