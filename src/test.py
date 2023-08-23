df8a234744112bffa0255db4a12110b9adbc9b31
Ruben Lopez <rjlopez2@gmail.com>
2023-08-23 23:30:43 +0200

    add default shape for testing

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@
 import napari
 import napari_omaas
 from napari_omaas import utils
+import numpy as np
 
 # import myModule
 # import importlib
@@ -25,6 +26,12 @@ inv_data = utils.invert_signal(viewer.layers[-1].data)
 ##### normalize data #####
 norm_data = utils.local_normal_fun(inv_data)
 viewer.add_image(norm_data, name=viewer.layers[-1].name  + "_Inv_Norm", colormap= "turbo", metadata= viewer.layers[-1].metadata)
+
+# add shape
+viewer.layers["ROI selection"].data = [np.array([[120.6049183 , 512.25410934],
+        [120.6049183 , 540.5934153 ],
+        [145.54350755, 540.5934153 ],
+        [145.54350755, 512.25410934]])]
 
 
 # viewer.open_sample("napari-omaas", "heartsample")
