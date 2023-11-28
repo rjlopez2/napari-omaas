e25b939eb8a9060b87d5a6527936e9ae31fb851d
Ruben Lopez <rjlopez2@gmail.com>
2023-11-28 10:25:18 +0100

    update test

--- src/test.py
+++ src/test.py
@@ -18,23 +18,62 @@ viewer = napari.Viewer()
 o = napari_omaas.OMAAS(viewer)
 # widg = napari_omaas.OMAAS.(viewer=viewer)  # or any other QWidget
 viewer.window.add_dock_widget(o, area='right')
-my_file  = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/20230504_17h-01m-53.sif"
-# my_file = "/Users/rubencito/programming_stuff/p_experimental/image_analysis/napari_play/OMAAS/test_data/single_scan/test_ruben/20230413_11h-57m-31s.sif"
+my_file  = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/20230504_17h-00m-43.sif"
 viewer.open(my_file, plugin= "napari-sif-reader")
-# ###### invert data #####
-# inv_data = utils.invert_signal(viewer.layers[-1].data)
-# ##### normalize data #####
-# norm_data = utils.local_normal_fun(inv_data)
-# viewer.add_image(norm_data, name=viewer.layers[-1].name  + "_Inv_Norm", colormap= "turbo", metadata= viewer.layers[-1].metadata)
-d = np.random.rand(500, 100 ,100)
-viewer.add_image(d)
 
+# my_file = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/average_stack.tif"
+# my_file = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/inv_normalize_stack.tif"
+# my_file = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/inv_normalize_stack_filtered.tif"
+# viewer.open(my_file, colormap = "turbo")
+
+
+# ##### invert and normalize data #####
+
+o.inv_and_norm_data_btn.click()
 
 # add shape
-# viewer.layers["ROI selection"].data = [np.array([[120.6049183 , 512.25410934],
-#         [120.6049183 , 540.5934153 ],
-#         [145.54350755, 540.5934153 ],
-#         [145.54350755, 512.25410934]])]
+
+viewer.add_shapes(data = [np.array([[120.6049183 , 512.25410934],
+        [120.6049183 , 540.5934153 ],
+        [145.54350755, 540.5934153 ],
+        [145.54350755, 512.25410934]])], name="shape_20230504_17h-00m-43")
+
+# make selections in the selectors
+o.listImagewidget.item(2).setSelected(True)
+o.listShapeswidget.item(0).setSelected(True)
+# plot
+o.plot_profile_btn.click()
+# average trace
+# o.create_average_AP_btn.click()
+
+
+# ###############################################################
+# # automatic load, invert and normalize the signal and
+# # then make the plotting
+# ###############################################################
+
+# # load a spool file
+# my_file = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/2_5Hz/20230710_12h-09m-00"
+# o.dir_box_text.setText(my_file)
+# o.load_spool_dir_btn.click()
+# # invert and normalize signal
+# o.inv_and_norm_data_btn.click()
+
+
+# # add shape
+
+# viewer.add_shapes(
+#     data = [np.array([[174.03193643, 460.55054341],
+#         [174.03193643, 488.88984937],
+#         [198.97052568, 488.88984937],
+#         [198.97052568, 460.55054341]])],
+#         name = "shape_20230710_12h-09m-0"
+# )
+# # make selections in the selectors
+# o.listImagewidget.item(2).setSelected(True)
+# o.listShapeswidget.item(0).setSelected(True)
+# # plot
+# o.plot_profile_btn.click()
 
 
 # viewer.open_sample("napari-omaas", "heartsample")
