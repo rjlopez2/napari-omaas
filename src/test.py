06af90480f8613f3288d2959f3ac145a0b0b8659
Ruben Lopez <rjlopez2@gmail.com>
2024-01-11 11:15:56 +0100

    test

--- src/test.py
+++ src/test.py
@@ -18,33 +18,82 @@ viewer = napari.Viewer()
 o = napari_omaas.OMAAS(viewer)
 # widg = napari_omaas.OMAAS.(viewer=viewer)  # or any other QWidget
 viewer.window.add_dock_widget(o, area='right')
-my_file  = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/20230504_17h-00m-43.sif"
-viewer.open(my_file, plugin= "napari-sif-reader")
+# my_file  = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/20230504_17h-00m-43.sif"
+# viewer.open(my_file, plugin= "napari-sif-reader")
 
 # my_file = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/average_stack.tif"
 # my_file = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/inv_normalize_stack.tif"
-# my_file = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/inv_normalize_stack_filtered.tif"
-# viewer.open(my_file, colormap = "turbo")
+my_file = "/Users/rubencito/Library/CloudStorage/OneDrive-UniversitaetBern/Bern/Odening_lab/OMAAS/test_data/for_APD/inv_normalize_stack_filtered.tif"
+viewer.open(my_file, colormap = "turbo")
 
+# viewer.layers.selection.active.visible = False
 
 # ##### invert and normalize data #####
 
-o.inv_and_norm_data_btn.click()
+# o.inv_and_norm_data_btn.click()
+# viewer.layers.selection.active.visible = False
+# viewer.layers.select_previous()
+# viewer.layers.selection.active.visible = False
+# viewer.layers.select_next()
 
 # add shape
 
-viewer.add_shapes(data = [np.array([[120.6049183 , 512.25410934],
-        [120.6049183 , 540.5934153 ],
-        [145.54350755, 540.5934153 ],
-        [145.54350755, 512.25410934]])], name="shape_20230504_17h-00m-43")
+# viewer.add_shapes(data = [np.array([[120.6049183 , 512.25410934],
+#         [120.6049183 , 540.5934153 ],
+#         [145.54350755, 540.5934153 ],
+#         [145.54350755, 512.25410934]])], name="shape_20230504_17h-00m-43")
+
+# viewer.layers[0].data = viewer.layers[0].data[:60]
 
 # make selections in the selectors
-o.listImagewidget.item(2).setSelected(True)
-o.listShapeswidget.item(0).setSelected(True)
-# plot
-o.plot_profile_btn.click()
-# average trace
+
+# o.listImagewidget.item(0).setSelected(True)
+# o.listShapeswidget.item(0).setSelected(True)
+# # plot
+# o.plot_profile_btn.click()
+
+# # average trace
+
+# # viewer.layers.select_previous()
+# o.preview_AP_splitted_btn.click()
+# # viewer.layers.selection.active.visible = False
 # o.create_average_AP_btn.click()
+# # viewer.layers.selection.active.visible = False
+
+# # # # filter trace + segment
+
+# # o.apply_spat_filt_btn.click()
+# # viewer.layers.selection.active.visible = False
+# # o.apply_temp_filt_btn.click()
+# # viewer.layers.selection.active.visible = False
+# o.apply_segmentation_btn.click()
+# # # # o.spat_filter_types.setCurrentText("Bilateral")
+
+
+# # # # make activation map
+
+# o.listImagewidget.item(0).setSelected(False)
+# o.listImagewidget.item(2).setSelected(True)
+
+# # maps_values =  [10, 25, 50, 75, 90]
+# # for value in maps_values:
+# #     o.slider_APD_map_percentage.setValue(value)
+# #     o.make_maps_btn.click()
+
+# o.toggle_map_type.setChecked(True)  
+# o.make_interpolation_check.setChecked(True)
+# o.make_maps_btn.click()
+
+# o.make_maps_btn.click()
+
+# make APD map
+
+# o.listImagewidget.item(0).setSelected(False)
+# o.listImagewidget.item(1).setSelected(True)
+
+# o.make_maps_btn.click()
+
+
 
 
 # ###############################################################
