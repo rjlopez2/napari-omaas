f8d39f6e8207c02a1ca0359a9d84beac002d4be3
Ruben Lopez <rjlopez2@gmail.com>
2024-08-14 09:33:03 +0200

    refactor to dynamically import CPU/GPU libraries

--- src/napari_omaas/utils.py
+++ src/napari_omaas/utils.py
@@ -1,5 +1,11 @@
+########## GUI libraries ##########
 from qtpy import QtCore, QtGui
-import numpy as np
+from qtpy.QtWidgets import QHBoxLayout, QVBoxLayout, QGroupBox, QGridLayout, QCheckBox
+from qtpy.QtCore import Qt, QAbstractTableModel, QModelIndex, QRect, QPropertyAnimation, QPoint, QEasingCurve, Property
+# from qtpy.QtCore import *
+from qtpy.QtGui import QColor, QPainter
+
+########## cv libraries ##########
 from skimage.filters import gaussian, threshold_triangle, median, rank, sobel
 from skimage.measure import label
 from skimage.filters.rank import mean_bilateral
@@ -7,30 +13,59 @@ from skimage.morphology import disk, binary_closing, remove_small_objects, closi
 from skimage.registration import optical_flow_ilk
 from skimage import transform, exposure, morphology, registration, segmentation
 from skimage.restoration import denoise_bilateral
+
+########## utils ##########
 import warnings
+import tqdm.auto as tqdm
+from time import time
+import pandas as pd
+
+########## napari & friends ##########
 from napari.layers import Image
+from napari.types import ImageData, ShapesData
 import sif_parser
 # from numba import njit
-import tqdm.auto as tqdm
 from napari.utils import progress
-from time import time
+from napari_macrokit import get_macro
 from optimap.image import detect_background_threshold
 from optimap import motion_compensate
 from optimap.video import normalize_pixelwise_slidingwindow, normalize_pixelwise
 
-from napari_macrokit import get_macro
-
+########## scientific computing libraries##########
 # from numba import jit, prange
 from scipy import signal, ndimage
 from scipy.interpolate import CubicSpline
 from scipy.ndimage import gaussian_filter, binary_fill_holes
-import cupy as cp
-from cupyx.scipy.ndimage import median_filter as cp_median_filter
-from cupyx.scipy.ndimage import gaussian_filter as cp_gaussian_filter
-# functions
+# import numpy as np
+# import cupy as cp
+# from cupyx.scipy.ndimage import median_filter as cp_median_filter
+# from cupyx.scipy.ndimage import gaussian_filter as cp_gaussian_filter
 
-from napari.types import ImageData, ShapesData
-import pandas as pd
+
+def import_gpu_or_cpu():
+    try:
+        import cupy as cp
+        from cupyx.scipy.ndimage import median_filter as cp_median_filter
+        from cupyx.scipy.ndimage import gaussian_filter as cp_gaussian_filter
+        return {
+            'array': cp,
+            'median_filter': cp_median_filter,
+            'gaussian_filter': cp_gaussian_filter,
+            'use_gpu': True
+        }
+    except ImportError:
+        import numpy as np
+        from scipy.ndimage import median_filter, gaussian_filter
+        return {
+            'array': np,
+            'median_filter': median_filter,
+            'gaussian_filter': gaussian_filter,
+            'use_gpu': False
+        }
+
+backend = import_gpu_or_cpu()
+
+
 # def detect_spots(
 #     image: "napari.types.ImageData",
 #     high_pass_sigma: float = 2,
@@ -41,10 +76,6 @@ import pandas as pd
 # import cupyx
 # from cucim.skimage import registration as registration_gpu
 # from cucim.skimage import transform as transform_gpu
-from qtpy.QtWidgets import QHBoxLayout, QVBoxLayout, QGroupBox, QGridLayout, QCheckBox
-from qtpy.QtCore import Qt, QAbstractTableModel, QModelIndex, QRect, QPropertyAnimation, QPoint, QEasingCurve, Property
-# from qtpy.QtCore import *
-from qtpy.QtGui import QColor, QPainter
 
 
 # instanciate a macro object
@@ -256,15 +287,27 @@ def apply_gaussian_func (data: "napari.types.ImageData",
     # return (gaussian(data, sigma))
     # return out_img
     # return gaussian_filter(data, sigma=sigma, order= 0, radius=kernel_size, # axes = (1,2))
-    data_cp = cp.asarray(data)
-    out_img = cp_gaussian_filter(data_cp, 
-                              sigma=(0, sigma, sigma), 
-                              order= 0,
-                              # radius=kernel_size,
-                              truncate=kernel_size,
-                              #axes = (1,2)
-                              )
-    return cp.asnumpy(out_img)
+
+
+    # data_cp = cp.asarray(data)
+    # out_img = cp_gaussian_filter(data_cp, 
+    #                           sigma=(0, sigma, sigma), 
+    #                           order= 0,
+    #                           # radius=kernel_size,
+    #                           truncate=kernel_size,
+    #                           #axes = (1,2)
+    #                           )
+    # return cp.asnumpy(out_img)
+
+    data_cp = backend['array'].asarray(data)
+    out_img = backend['gaussian_filter'](data_cp, 
+                                         sigma=(0, sigma, sigma), 
+                                         order=0,
+                                         truncate=kernel_size)
+    
+    return backend['array'].asnumpy(out_img) if backend['use_gpu'] else out_img
+
+    
 
 
 @macro.record
@@ -289,34 +332,46 @@ def apply_median_filt_func (data: "napari.types.ImageData",
        Smoothed Image with Median filter.
 
     """
+#     param = int(param)
+#     # data = image.active.data
+#     # out_img = np.empty_like(data)
+#     footprint = disk(int(param))
+
+#     # print(f'applying "apply_median_filt_func" to image {image.active}')
+
+#     # for plane, img in enumerate(data):
+#         # out_img[plane] = median(img, footprint = footprint)
+
+#     # for plane in list(range(data.shape[0])):
+#     #     out_img[plane, :, :] = median(data[plane, :, :], footprint = footprint)
+
+# # using numba method #
+#     # out_img = parallel_median(data,footprint)
+#     # for plane, img in enumerate(data):
+#     #     out_img[plane] = signal.medfilt2d(img, kernel_size = param)
+    
+#     # out_img = ndimage.median_filter(data, size = (1, param, param))
+
+    
+#     data_cp = cp.asarray(data)
+#     # xp = cpx.get_array_module(data_cp, param)  # 'xp' is a standard usage in the community
+#     # print("Using:", xp.__name__)
+#     out_img = cp_median_filter(data_cp, size = (1, param, param))
+    
+#     return cp.asnumpy(out_img)
+
+
+
     param = int(param)
-    # data = image.active.data
-    # out_img = np.empty_like(data)
-    footprint = disk(int(param))
-
-    # print(f'applying "apply_median_filt_func" to image {image.active}')
-
-    # for plane, img in enumerate(data):
-        # out_img[plane] = median(img, footprint = footprint)
-
-    # for plane in list(range(data.shape[0])):
-    #     out_img[plane, :, :] = median(data[plane, :, :], footprint = footprint)
-
-# using numba method #
-    # out_img = parallel_median(data,footprint)
-    # for plane, img in enumerate(data):
-    #     out_img[plane] = signal.medfilt2d(img, kernel_size = param)
     
-    # out_img = ndimage.median_filter(data, size = (1, param, param))
-
+    # Convert data to GPU array if on GPU, else use CPU array
+    data_cp = backend['array'].asarray(data)
     
-    data_cp = cp.asarray(data)
-    # xp = cpx.get_array_module(data_cp, param)  # 'xp' is a standard usage in the community
-    # print("Using:", xp.__name__)
-    out_img = cp_median_filter(data_cp, size = (1, param, param))
+    # Apply the filter using the appropriate backend
+    out_img = backend['median_filter'](data_cp, size=(1, param, param))
     
-    return cp.asnumpy(out_img)
-
+    # Convert back to CPU array if needed
+    return backend['array'].asnumpy(out_img) if backend['use_gpu'] else out_img
 
 @macro.record
 def pick_frames_fun(
