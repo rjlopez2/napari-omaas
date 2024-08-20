e1a1050d6d13ba22fcecf49319fd10d9b70741fe
Ruben Lopez <rjlopez2@gmail.com>
2024-08-20 09:08:57 +0200

    add spinbox to allow user to adjust windows size in slide window  normalization method

--- src/napari_omaas/_widget.py
+++ src/napari_omaas/_widget.py
@@ -157,22 +157,31 @@ class OMAAS(QWidget):
         self.pre_processing_group = VHGroup('Pre-porcessing', orientation='G')
 
         ######## pre-processing btns ########
-        self.inv_and_norm_data_btn = QPushButton("Invert + Normalize")        
-        self.inv_and_norm_data_btn.setToolTip(("Invert and Apply Normalization to the current Image."))
-        self.pre_processing_group.glayout.addWidget(self.inv_and_norm_data_btn, 1, 1, 1, 1)
 
-        self.inv_data_btn = QPushButton("Invert signal")
-        self.inv_data_btn.setToolTip(("Invert the polarity of the signal"))
-        self.pre_processing_group.glayout.addWidget(self.inv_data_btn , 2, 1, 1, 1)
-
-        self.apply_normalization_btn = QPushButton("Normalize")        
+        self.apply_normalization_btn = QPushButton("Normalize")
         self.apply_normalization_btn.setToolTip(("Apply Normalization to the current Image."))
-        self.pre_processing_group.glayout.addWidget(self.apply_normalization_btn, 1, 2, 1, 1)
+        self.pre_processing_group.glayout.addWidget(self.apply_normalization_btn, 1, 1, 1, 1)
 
         self.data_normalization_options = QComboBox()
         self.data_normalization_options.addItems(["Local max", "Slide window", "Global"])
         self.data_normalization_options.setToolTip(("List of normalization methods."))
-        self.pre_processing_group.glayout.addWidget(self.data_normalization_options, 1, 3, 1, 1)
+        self.pre_processing_group.glayout.addWidget(self.data_normalization_options, 1, 2, 1, 1)
+
+        self.inv_and_norm_data_btn = QPushButton("Invert + Normalize")        
+        self.inv_and_norm_data_btn.setToolTip(("Invert and Apply Normalization to the current Image."))
+        self.pre_processing_group.glayout.addWidget(self.inv_and_norm_data_btn, 1, 3, 1, 1)
+        
+        self.inv_data_btn = QPushButton("Invert signal")
+        self.inv_data_btn.setToolTip(("Invert the polarity of the signal"))
+        self.pre_processing_group.glayout.addWidget(self.inv_data_btn , 2, 1, 1, 1)
+
+        self.slide_wind_n = QSpinBox()
+        self.slide_wind_n.setToolTip(("Windows size for slide window normalization method."))
+        # self.slide_wind_n.setSingleStep(1)
+        self.slide_wind_n.setValue(100)
+        self.slide_wind_n.setMaximum(10000000)
+        self.pre_processing_group.glayout.addWidget(self.slide_wind_n , 2, 2, 1, 1)
+
 
         # self.splt_chann_label = QLabel("Split Channels")
         # self.pre_processing_group.glayout.addWidget(self.splt_chann_label, 3, 6, 1, 1)
@@ -1321,8 +1330,9 @@ class OMAAS(QWidget):
 
                 elif type_of_normalization == normalization_methods[1]:
                     print(f'computing "{type_of_normalization}" to image {current_selection}')
-                    results = slide_window_normalization_func(current_selection.data)
-                    self.add_result_img(result_img=results, single_label_sufix="SliWind20", add_to_metadata = "SliWind_norm_signal")
+                    wind_size = self.slide_wind_n.value()
+                    results = slide_window_normalization_func(current_selection.data, slide_window=wind_size)
+                    self.add_result_img(result_img=results, single_label_sufix=f"SliWind{wind_size}", add_to_metadata = "SliWind_norm_signal")
                     self.add_record_fun()
 
                 elif type_of_normalization == normalization_methods[2]:
